1790f67ee138290900e894c8963db529952fc09b
Nader Nagy <NaderNagiSamy@outlook.com>
2023-08-07 10:22:47 +0300

    Redesign Code.

--- SearchablePDF/Form1.cs
+++ SearchablePDF/Form1.cs
@@ -1,17 +1,16 @@
-﻿using BitMiracle.Docotic.Pdf;
+﻿using Ghostscript.NET;
+using Ghostscript.NET.Rasterizer;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI.WebControls;
 using System.Windows.Forms;
 using Tesseract;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace SearchablePDF
 {
@@ -23,66 +22,120 @@ namespace SearchablePDF
             InitializeComponent();
         }
 
+
+
         private void ConvertButton_Click(object sender, EventArgs e)
         {
-            BitMiracle.Docotic.LicenseManager.AddLicenseData("4N7N9-JTDT7-GII7F-HJ62K-FHL85");
-
             var docuentFileName = FilePathLabel.Text;
             PageConfidenceListBox.Items.Clear();
             if (Path.GetExtension(docuentFileName).ToLower().Equals(".pdf"))
             {
-                using (var pdf = new PdfDocument(docuentFileName))
+                try
                 {
-                    using (var engine = new TesseractEngine($@"{AppDomain.CurrentDomain.BaseDirectory}\tessdata\", "ara", EngineMode.Default))
+
+                    GhostscriptPngDevice dev = new GhostscriptPngDevice(GhostscriptPngDeviceType.Png16m);
+                    dev.GraphicsAlphaBits = GhostscriptImageDeviceAlphaBits.V_4;
+                    dev.TextAlphaBits = GhostscriptImageDeviceAlphaBits.V_4;
+                    dev.InputFiles.Add(docuentFileName);
+                    dev.DownScaleFactor = 2;
+                    dev.CustomSwitches.Add("-dDOINTERPOLATE"); // custom parameter
+                    dev.CustomSwitches.Add("-r500*500");
+                    dev.OutputPath = $@"{Path.GetDirectoryName(docuentFileName)}\indispensable_color_page_%03d.png";
+                    dev.Process();
+
+
+                    foreach (var filePath in Directory.GetFiles(Path.GetDirectoryName(docuentFileName),
+                        "*.png", SearchOption.AllDirectories).ToList())
                     {
-                        Parallel.For(0, pdf.PageCount,
-                   i =>
-                   {
-                       lock (pdf)
-                       {
-                           lock (engine)
-                           {
-                               PdfPage page = pdf.Pages[i];
-                               string searchableText = page.GetText();
+                        using (var engine = new TesseractEngine($@"{AppDomain.CurrentDomain.BaseDirectory}\tessdata\", "ara", EngineMode.Default))
+                        {
+                            using (var pix = Pix.LoadFromFile(filePath))
+                            {
+                                using (Page recognizedPage = engine.Process(pix))
+                                {
+                                    string recognizedText = recognizedPage.GetText();
+                                    PageConfidenceListBox.Items.Add($"Mean confidence: {recognizedPage.GetMeanConfidence():P}");
+                                    using (var writer = new StreamWriter($@"{Path.GetDirectoryName(filePath)}\{Path.GetFileNameWithoutExtension(filePath)}.txt"))
+                                    {
+                                        writer.Write(recognizedText.Normalize());
+                                    }
+                                }
 
-                               // Simple check if the page contains searchable text.
-                               // We do not need to perform OCR in that case.
-                               if (!string.IsNullOrEmpty(searchableText.Trim()))
-                               {
-                                   return;
-                               }
-
-                               // This page is not searchable.
-                               // Save the page as a high-resolution image
-                               PdfDrawOptions options = PdfDrawOptions.Create();
-                               options.BackgroundColor = new PdfRgbColor(255, 255, 255);
-                               options.HorizontalResolution = 300;
-                               options.VerticalResolution = 300;
-
-                               string pageImage = $"page_{i}.png";
-                               page.Save(pageImage, options);
-
-                               // Perform OCR
-                               using (Pix img = Pix.LoadFromFile(pageImage))
-                               {
-                                   using (Page recognizedPage = engine.Process(img))
-                                   {
-                                       string recognizedText = recognizedPage.GetText();
-                                       using (var writer = new StreamWriter($@"{Path.GetDirectoryName(docuentFileName)}\page_{i}.txt"))
-                                           writer.Write(recognizedText.ToString());
-
-                                   }
-                               }
-
-                               File.Delete(pageImage);
+                            }
 
 
-                           }
-                       }
-                   });
+                        }
+
+
+                        Console.WriteLine(filePath);
                     }
+
+                }
+                catch (Exception)
+                {
+                    throw;
                 }
             }
+
+
+            //BitMiracle.Docotic.LicenseManager.AddLicenseData("4N7N9-JTDT7-GII7F-HJ62K-FHL85");
+
+            //var docuentFileName = FilePathLabel.Text;
+            //PageConfidenceListBox.Items.Clear();
+            //if (Path.GetExtension(docuentFileName).ToLower().Equals(".pdf"))
+            //{
+            //    using (var pdf = new PdfDocument(docuentFileName))
+            //    {
+            //        using (var engine = new TesseractEngine($@"{AppDomain.CurrentDomain.BaseDirectory}\tessdata\", "ara", EngineMode.Default))
+            //        {
+            //            Parallel.For(0, pdf.PageCount,
+            //       i =>
+            //       {
+            //           lock (pdf)
+            //           {
+            //               lock (engine)
+            //               {
+            //                   PdfPage page = pdf.Pages[i];
+            //                   string searchableText = page.GetText();
+
+            //                   // Simple check if the page contains searchable text.
+            //                   // We do not need to perform OCR in that case.
+            //                   if (!string.IsNullOrEmpty(searchableText.Trim()))
+            //                   {
+            //                       return;
+            //                   }
+
+            //                   // This page is not searchable.
+            //                   // Save the page as a high-resolution image
+            //                   PdfDrawOptions options = PdfDrawOptions.Create();
+            //                   options.BackgroundColor = new PdfRgbColor(255, 255, 255);
+            //                   options.HorizontalResolution = 300;
+            //                   options.VerticalResolution = 300;
+
+            //                   string pageImage = $"page_{i}.png";
+            //                   page.Save(pageImage, options);
+
+            //                   // Perform OCR
+            //                   using (Pix img = Pix.LoadFromFile(pageImage))
+            //                   {
+            //                       using (Page recognizedPage = engine.Process(img))
+            //                       {
+            //                           string recognizedText = recognizedPage.GetText();
+            //                           using (var writer = new StreamWriter($@"{Path.GetDirectoryName(docuentFileName)}\page_{i}.txt"))
+            //                               writer.Write(recognizedText.ToString());
+
+            //                       }
+            //                   }
+
+            //                   File.Delete(pageImage);
+
+
+            //               }
+            //           }
+            //       });
+            //        }
+            //    }
+            //}
 
         }
 
@@ -99,7 +152,6 @@ namespace SearchablePDF
             }
 
         }
-
 
     }
 }
